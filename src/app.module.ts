ca9080f25f708fdbb6d51ae32a5c16d4f27ea0a9
Sagyam Thapa <sagyamthapa32@gmail.com>
2023-12-27 06:58:45 +0545

    feat(app): Add new resource coin

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CoinModule } from './coin/coin.module';
 
 @Module({
-  imports: [],
+  imports: [CoinModule],
   controllers: [AppController],
   providers: [AppService],
 })
