c15df78a53cf6ea1fe6c7537bb37aee487ed6d27
Sagyam Thapa <sagyamthapa32@gmail.com>
2023-12-27 08:23:24 +0545

    chore(app): Enable CORS, Hemlet and RateLimiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,38 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoinModule } from './coin/coin.module';
 
 @Module({
-  imports: [CoinModule],
+  imports: [
+    CoinModule,
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 1000,
+        limit: 5,
+      },
+      {
+        name: 'medium',
+        ttl: 10000,
+        limit: 20,
+      },
+      {
+        name: 'long',
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost:4000', 'https://bitmosys-q423.vercel.app'],
+  });
+
+  app.use(helmet());
+
+  const config = new DocumentBuilder()
+    .setTitle('Coins API')
+    .setDescription('The coins API description')
+    .setVersion('1.0')
+    .addTag('coins')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
-bootstrap();
+
+bootstrap()
+  .then(() => console.log('Application is listening on port 3000'))
+  .catch((err) => console.error(err));
